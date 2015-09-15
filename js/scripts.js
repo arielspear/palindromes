77124b0ddbfa6464f754d6fd364cd9889b0a40aa
juliana suzuki & Ariel Spear <student@epicodus.com>
2015-09-15 09:47:13 -0700

    test reverse method

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var arrayMaker = function(input) {
+  var inputArray = input.split("");
+  return inputArray;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe('arrayMaker', function() {
   it("it turns a string into an array split on characters", function() {
-      expect(arrayMaker("kitten")).to.equal(['k', 'i', 't', 't', 'e', 'n']);
+      expect(arrayMaker("kitten")).to.eql(['k', 'i', 't', 't', 'e', 'n']);
+  })
+});
+
+describe('reverse', function() {
+  it("it reverses the chars in the array", function() {
+      expect(reverse(['p', 'u', 'p', 'p', 'y'])).to.eql(['y', 'p', 'p', 'u', 'p']);
   })
 });
